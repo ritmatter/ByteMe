a9ebafcec97cb4a9e1fb275a0074008b7d316c6d
ritmatter <mritter123@gmail.com>
2014-07-26 14:15:14 -0700

    Fix hash map code

--- hash_map.py
+++ hash_map.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import pickle
 
+#words = ["a", "b", "apple", "pear", "banana"]
+#SMALL_MAP_SIZE = 1
+#LARGE_MAP_SIZE = 2
 words = pickle.load( open( "sortedWords.p", "rb" ) )
 SMALL_MAP_SIZE = 63
 LARGE_MAP_SIZE = 16383
@@ -17,11 +20,30 @@ while i < SMALL_MAP_SIZE:
     file.write("{\"" + words[i] + "\"," + str(i) + "},")
     i += 1
 file.write("{\"" + words[i] + "\"," + str(i) + "}};\n")
+i += 1
 
+j = 0
 file.write("std::map<std::string, int> long_map = {")
-while i < LARGE_MAP_SIZE:
-    file.write("{\"" + words[i] + "\"," + str(i) + "},")
+while j < LARGE_MAP_SIZE:
+    file.write("{\"" + words[i] + "\"," + str(j) + "},")
     i += 1
-file.write("{\"" + words[i] + "\"," + str(i) + "}};")
+    j += 1
+file.write("{\"" + words[i] + "\"," + str(j) + "}};\n")
+
+i = 0
+file.write("const string small_decode_array[] = {")
+while i < SMALL_MAP_SIZE:
+    file.write("\"" + words[i] + "\",")
+    i += 1
+file.write("\"" + words[i] + "\"};\n")
+i+= 1
+
+j = 0
+file.write("const string large_decode_array[] = {")
+while j < LARGE_MAP_SIZE:
+    file.write("\"" + words[i] + "\",")
+    i += 1
+    j += 1
+file.write("\"" + words[i] + "\"};")
 
 file.close()
