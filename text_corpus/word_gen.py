c657b9f88e1d33f8e58c814cf4061d64987f0077
ritmatter <mritter123@gmail.com>
2014-07-26 17:19:42 -0700

    Create ranked word map
    Conflicts:
    	Makefile

--- text_corpus/word_gen.py
+++ text_corpus/word_gen.py
@@ -10,6 +10,9 @@ mypath = path.dirname(path.abspath(__file__))
 txtfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 txtfiles = [f for f in txtfiles if f.endswith(suffix)]
 
+def score(freq, length):
+    return 0.9 * freq + 0.1 * length
+
 word_list = []
 
 for f in txtfiles:
@@ -25,8 +28,12 @@ for word in word_list:
 	else:
 		word_freq[word] = 1
 
-sorted_words = [word for word in word_freq.keys() if len(word) > 1]
-sorted_words = sorted(sorted_words, key=lambda x: -word_freq[x])
+word_score = {}
+for word in word_freq.keys():
+    word_score[word] = score(word_freq[word], len(word))
+
+sorted_words = [word for word in word_score.keys() if len(word) > 1]
+sorted_words = sorted(sorted_words, key=lambda x: -word_score[x])
 
 small = sorted_words[:64]
 large = sorted_words[64:]
